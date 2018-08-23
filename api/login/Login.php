71d59faa4e7a413f0665b62a1e94c3f627d23565
Dsblima <dsbl@ecomp.poli.br>
2018-08-22 23:43:58 -0300

    Inicio da modelagem do projeto

---
+++ api/login/Login.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: casa
+ * Date: 8/22/2018
+ * Time: 10:54 PM
+ */
+
+class Login
+{
+
+}
---
+++ api/login/index.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: casa
+ * Date: 8/22/2018
+ * Time: 10:53 PM
+ */
